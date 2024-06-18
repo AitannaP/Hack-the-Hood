2c67ef7e8a3360b487b543d316bc4def92920e6e
Aitanna Parker <aitannaparker@gmail.com>
2024-06-18 13:44:46 -0700

    adding final update

--- hack the hood/lab2/git_practice.py
+++ hack the hood/lab2/git_practice.py
@@ -1,1 +1,16 @@
 print ("hello world")
+
+name = "LeBron"
+age = 35
+salary = 37.44
+plays_basketball = True
+jersey_number = "23"
+
+car_name= "Toyota"
+
+x=50
+
+my_first_name = "John"
+
+print(name, type(name), age, type(age), salary,type(salary), plays_basketball, type(plays_basketball),
+       jersey_number,type(jersey_number), car_name, type(car_name), x , type(x), my_first_name, type(my_first_name))
